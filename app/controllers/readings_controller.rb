7b1e0d94fd820485ec92f5cc50ea4e1400e21aa8
tmblum <trevorblum@Trevors-MacBook-Air.local>
2015-04-28 21:56:14 -0700

    Skip csrf check

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,4 +1,5 @@
 class ReadingsController < ApplicationController
+	skip_before_filter :verify_authenticity_token, :only => [:create]
   def create
   	reading = Reading.create(reading_params)
   	respond_to do |f|
